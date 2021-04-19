8ef4bc590542dda37c17f23445b147d31bb039b6
Rishabh Dugaye <58358984+rishabhd786@users.noreply.github.com>
2021-04-19 17:13:21 +0530

    Delete Assignment_1_and_2/Assign1/Q1 directory

--- Assignment_1_and_2/Assign1/Q1/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chmod +777 5688bc90-ab36-4a02-b192-d2426572bc93
-PKGNAME=$(./5688bc90-ab36-4a02-b192-d2426572bc93 | sed '1q;d' |awk '{print $3}')
-F1=$(ls -c1 | sed '2q;d' |awk '{print $1}' )
-F2=$(ls -c1 | sed '3q;d' |awk '{print $1}' )
-chmod +777 deploy $F1 $F2
-./deploy $F1 $PKGNAME $F2 $1 >> submission-q1.txt
